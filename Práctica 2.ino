5855e713d03c667fafcbbc1084375cd3c32e5c5d
Alexandergonzalez10 <166849511+Alexandergonzalez10@users.noreply.github.com>
2024-07-18 00:29:10 -0600

    Update Práctica 2.ino

--- Práctica 2.ino
+++ Práctica 2.ino
@@ -1,5 +1,5 @@
 /* 
-	Fundación Kinal
+  Fundación Kinal
     Centro educativo Técncico Laboral Kinal
     Quinto Perito
     Quinto Electrónica 
@@ -10,66 +10,73 @@
     Fecha: 16 de julio de 2024
 */
 
-#include <Wire.h>
-#include <Adafruit_Sensor.h>
-#include <Adafruit_ADXL345_U.h>
-#include <SoftwareSerial.h>
-#include <DFRobotDFPlayerMini.h>
+// Librerías
+#include <Wire.h>                   // Librería para usar el protocolo i2c
+#include <SparkFun_ADXL345.h>       // Librería para usar el acelerómetro ADXL345
+#include <SoftwareSerial.h>         // Librería para emular otro puerto serie
+#include <DFRobotDFPlayerMini.h>    // Librería para usar el reproductor MP3
 
-// Definición de pines
+// Constructores
+ADXL345 acc_sismo = ADXL345();          // Constructor para usar el acelerómetro
+SoftwareSerial mp3_sismo(A0, A1);       // RX A0 y TX A1
+DFRobotDFPlayerMini reproductor_MP3;    // Constructor para el reproductor MP3
+
+// Definición de pines para LEDs
 #define LED_BAR_START 2
 #define LED_BAR_END 11
 
-// Configuración del DFPlayer Mini
-SoftwareSerial mySerial(7, 8); // RX, TX
-DFRobotDFPlayerMini player;
+// Variables
+unsigned int acc_X;         // Aceleración en el eje x
+unsigned int acc_Y;         // Aceleración en el eje y
+unsigned int acc_Z;         // Aceleración en el eje z
 
-// Objeto para el acelerómetro ADXL345
-Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+// Variable donde guardo la magnitud del vector aceleración
+float magnitud_acc;
 
+// Configuraciones
 void setup() {
-  // Inicialización de comunicación serial para el DFPlayer Mini
-  mySerial.begin(9600);
-  if (!player.begin(mySerial)) {
-    Serial.println(F("No se pudo encontrar el módulo DFPlayer Mini"));
-    while(true);
-  }
-  Serial.println(F("DFPlayer Mini listo"));
+  Serial.begin(9600);              // Inicio la comunicación serial del Arduino hacia la computadora
+  mp3_sismo.begin(9600);           // Inicio la comunicación serial emulada para el mp3
+  acc_sismo.powerOn();             // Enciendo el acelerómetro
+  acc_sismo.setRangeSetting(8);    // Configuro la sensibilidad del acelerómetro
 
-  // Inicialización del acelerómetro ADXL345
-  if(!accel.begin()) {
-    Serial.println(F("No se pudo encontrar el acelerómetro ADXL345"));
-    while(true);
+  // Compruebo si hay conexión con el módulo mp3
+  if (!reproductor_MP3.begin(mp3_sismo)) { 
+    // Si no existe, me quedo aquí siempre. Ya no avanzo en el código
+    Serial.println("No he podido comunicarme con el modulo MP3");
+    while (1);
   }
-  Serial.println(F("ADXL345 listo"));
+  
+  // Si la comunicación ha sido exitosa, avanzo
+  reproductor_MP3.volume(10);       // Volumen al máximo
+  Serial.println("Comunicacion exitosa con el reproductor MP3");
+  Serial.println("Practica 2");
+  Serial.println("Reproduzco la primera cancion");
+  reproductor_MP3.play(1);          // Reproduzco la primera canción
+  delay(100);
 
   // Configuración de pines de salida para la barra de LEDs
   for (int i = LED_BAR_START; i <= LED_BAR_END; i++) {
     pinMode(i, OUTPUT);
+    digitalWrite(i, LOW);           
   }
 }
 
 void loop() {
-  sensors_event_t event;
-  accel.getEvent(&event);
+  // Leo las variables medidas por el acelerómetro
+  acc_sismo.readAccel(&acc_X, &acc_Y, &acc_Z);
 
-  // Obtener la aceleración en los ejes X, Y, Z
-  float ax = event.acceleration.x;
-  float ay = event.acceleration.y;
-  float az = event.acceleration.z;
-
-  // Calcular la magnitud de la aceleración
-  float magnitude = sqrt(ax * ax + ay * ay + az * az);
-
-  // Determinar la intensidad del sismo según la magnitud
-  int intensity = map((int)magnitude, 0, 200, 0, 10); 
+  // Obtengo la magnitud del vector aceleración
+  magnitud_acc = sqrtf((acc_X * acc_X) + (acc_Y * acc_Y) + (acc_Z * acc_Z));
+  
+  int mapeo_intensidad = map(magnitud_acc, 47, 75, 0, 10);
 
   // Mostrar la intensidad en la barra de LEDs
-  showIntensity(intensity);
+  showIntensity(mapeo_intensidad);
 
   // Activar alarma sonora si la intensidad es mayor a 6 (nivel VI en escala de Mercalli)
-  if (intensity > 6) {
-    player.play(1); // Número de archivo de sonido en la tarjeta SD
+  if (mapeo_intensidad > 6) {
+    reproductor_MP3.play(1); // Número de archivo de sonido en tu tarjeta SD
   }
 
   delay(1000); // Intervalo de lectura y procesamiento
@@ -81,9 +88,9 @@ void showIntensity(int intensity) {
   for (int i = LED_BAR_START; i <= LED_BAR_END; i++) {
     digitalWrite(i, LOW);
   }
-
-  // Enciende LEDs según la intensidad
-  for (int i = LED_BAR_START; i <= LED_BAR_START + intensity - 1; i++) {
+  
+  // Enciende los LEDs según la intensidad
+  for (int i = LED_BAR_START; i < LED_BAR_START + intensity; i++) {
     digitalWrite(i, HIGH);
   }
 }
